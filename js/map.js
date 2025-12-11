b93894191de6642d6fe32b9c4636b42dd6950499
juhku1 <61950305+juhku1@users.noreply.github.com>
2025-12-11 15:33:03 +0000

    Set initial view from Estonia towards Helsinki

--- js/map.js
+++ js/map.js
@@ -10,10 +10,10 @@
 const map = new maplibregl.Map({
   container: 'map',
   style: 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json',
-  center: [22.0, 60.2],
-  zoom: 6,
+  center: [24.9, 59.5],  // Gulf of Finland, view from Estonia towards Helsinki
+  zoom: 8,
   pitch: 60,
-  bearing: 0,
+  bearing: 340,  // Northwest direction towards Helsinki
   attributionControl: false
 });
 
